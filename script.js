d2cdb366bbf256edb98b11ad8c6346f06ed38760
Rahmanbek <gassulan@gmail.com>
2025-12-31 01:11:54 +0500

    Refactor weather fetching and error handling
    Updated error handling and city name display.

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ const apiKey = 'd720c9d44ee07cf5696d77650ccfbca3';
 const kzCities = ['Almaty', 'Astana', 'Shymkent', 'Aktau', 'Atyrau', 'Aktobe', 'Karaganda', 'Taraz', 'Pavlodar', 'Semey'];
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Сайт ашылғанда бірден рандомды қаланы шығару
     const randomCity = kzCities[Math.floor(Math.random() * kzCities.length)];
     getWeather(randomCity);
 });
 
 async function getWeather(city) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=kk`;
+    
     try {
         const response = await fetch(url);
-        if (!response.ok) throw new Error('Дерек алу қатесі');
+        if (!response.ok) throw new Error('Қате!');
+        
         const data = await response.json();
         
-        document.getElementById('cityName').innerText = data.name;
+        document.getElementById('cityName').innerText = data.name.toUpperCase();
         document.getElementById('temperature').innerText = `${Math.round(data.main.temp)}°C`;
         document.getElementById('humidity').innerText = data.main.humidity;
     } catch (error) {
-        document.getElementById('cityName').innerText = "Қате орын алды";
+        document.getElementById('cityName').innerText = "АҚАУ ШЫҚТЫ";
+        console.error(error);
     }
 }
